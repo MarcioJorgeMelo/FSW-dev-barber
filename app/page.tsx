e6ca796f6d7d250d444d5513ca7204d5f0b438bb
MarcioJorgeMelo <marciojorgemedeirosmelofilho@gmail.com>
2025-05-19 19:31:24 -0300

    feat: add header and banner on page and create input search

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,37 @@
+import Header from "@/components/header"
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Search } from "lucide-react"
+import Image from "next/image"
+import bannerImage from "@/public/bannerfswbarber.png"
 
 export default function Home() {
   return (
     <div>
-      <h1 className="mb-5 text-red-500">Olá</h1>
-      <Button>Clica aqui!</Button>
+      <Header />
+
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Jorge!</h2>
+
+        <p>Segunda, 19 de Maio</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+
+          <Button>
+            <Search />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src={bannerImage}
+            alt="Agende nos melhores com FSW Barber"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
     </div>
   )
 }
