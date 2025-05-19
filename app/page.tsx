24d3554ff86773eea5236591482cc74e14e2082c
MarcioJorgeMelo <marciojorgemedeirosmelofilho@gmail.com>
2025-05-19 19:48:47 -0300

    feat: create appointment card on page

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ import { Input } from "@/components/ui/input"
 import { Search } from "lucide-react"
 import Image from "next/image"
 import bannerImage from "@/public/bannerfswbarber.png"
+import { Card, CardContent } from "@/components/ui/card"
+import { Badge } from "@/components/ui/badge"
+import { Avatar, AvatarImage } from "@/components/ui/avatar"
 
 export default function Home() {
   return (
@@ -31,6 +34,34 @@ export default function Home() {
             className="rounded-xl object-cover"
           />
         </div>
+
+        <h2 className="text-xl font-bold">Agendamentos</h2>
+
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                </Avatar>
+
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Maio</p>
+
+              <h3 className="text-2xl">19</h3>
+
+              <p className="text-sm">19:45</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
