3bc6b07b4bfef5d8c52cae55389e7e4e176d90bd
MarcioJorgeMelo <marciojorgemedeirosmelofilho@gmail.com>
2025-05-21 20:55:40 -0300

    feat: add link to home on logo image

--- components/header.tsx
+++ components/header.tsx
@@ -5,12 +5,20 @@ import { Sheet, SheetTrigger } from "./ui/sheet"
 import { Button } from "./ui/button"
 import { MenuIcon } from "lucide-react"
 import SidebarSheet from "./sidebar-sheet"
+import Link from "next/link"
 
 const Header = () => {
   return (
     <Card>
       <CardContent className="flex items-center justify-between p-5">
-        <Image src={LogoImg} alt="Logo do FSW Barber" height={18} width={120} />
+        <Link href="/">
+          <Image
+            src={LogoImg}
+            alt="Logo do FSW Barber"
+            height={18}
+            width={120}
+          />
+        </Link>
 
         <Sheet>
           <SheetTrigger asChild>
