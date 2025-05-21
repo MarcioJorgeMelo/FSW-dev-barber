177b07fab62a2fc685a24aad66c720bc0e0cbb1d
MarcioJorgeMelo <marciojorgemedeirosmelofilho@gmail.com>
2025-05-21 18:41:20 -0300

    feat: create a sheet to interactive nav menu on home page

--- components/header.tsx
+++ components/header.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image"
 import { Card, CardContent } from "./ui/card"
 import LogoImg from "@/public/logofswbarber.svg"
+import { Sheet, SheetTrigger } from "./ui/sheet"
 import { Button } from "./ui/button"
 import { MenuIcon } from "lucide-react"
+import SidebarSheet from "./sidebar-sheet"
 
 const Header = () => {
   return (
@@ -10,9 +12,15 @@ const Header = () => {
       <CardContent className="flex items-center justify-between p-5">
         <Image src={LogoImg} alt="Logo do FSW Barber" height={18} width={120} />
 
-        <Button size="icon" variant="outline">
-          <MenuIcon />
-        </Button>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size="icon" variant="outline">
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+
+          <SidebarSheet />
+        </Sheet>
       </CardContent>
     </Card>
   )
