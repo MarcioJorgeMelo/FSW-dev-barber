9215ae56e0c74fba765f56dbbb159eb393099dfa
MarcioJorgeMelo <marciojorgemedeirosmelofilho@gmail.com>
2025-05-22 18:27:46 -0300

    feat: pass barbershop to ServiceItem

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -92,7 +92,11 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
 
         <div className="space-y-3">
           {barbershop.services.map((service) => (
-            <ServiceItem key={service.id} service={service} />
+            <ServiceItem
+              key={service.id}
+              service={service}
+              barberShop={barbershop}
+            />
           ))}
         </div>
       </div>
