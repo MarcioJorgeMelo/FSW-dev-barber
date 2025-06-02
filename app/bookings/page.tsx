092e1371a6c66b2dc9ca16bbe42402c84637e42d
MarcioJorgeMelo <marciojorgemedeirosmelofilho@gmail.com>
2025-06-02 17:50:46 -0300

    feat: create Bookings page

---
+++ app/bookings/page.tsx
@@ -0,0 +1,83 @@
+import Header from "@/components/header"
+import { db } from "@/lib/prisma"
+import { getServerSession } from "next-auth"
+import { authOptions } from "../api/auth/[...nextauth]/route"
+import { notFound } from "next/navigation"
+import BookingItem from "@/components/bookingItem"
+
+const Bookings = async () => {
+  const session = await getServerSession(authOptions)
+
+  if (!session?.user) {
+    return notFound()
+  }
+
+  const confirmedBookings = await db.booking.findMany({
+    where: {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      userId: (session.user as any).id,
+      date: {
+        gte: new Date(),
+      },
+    },
+    include: {
+      service: {
+        include: {
+          barberShop: true,
+        },
+      },
+    },
+  })
+
+  const concludedBookings = await db.booking.findMany({
+    where: {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      userId: (session.user as any).id,
+      date: {
+        lte: new Date(),
+      },
+    },
+    include: {
+      service: {
+        include: {
+          barberShop: true,
+        },
+      },
+    },
+    orderBy: {
+      date: "asc",
+    },
+  })
+
+  return (
+    <>
+      <Header />
+
+      <div className="p-5">
+        <h1 className="text-xl font-bold">Agendamentos</h1>
+
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Confirmados
+        </h2>
+
+        <div className="space-y-3">
+          {confirmedBookings.map((booking) => (
+            <BookingItem key={booking.id} booking={booking} />
+          ))}
+        </div>
+
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Finalizados
+        </h2>
+
+        <div className="space-y-3">
+          {concludedBookings.map((booking) => (
+            <BookingItem key={booking.id} booking={booking} />
+          ))}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Bookings
