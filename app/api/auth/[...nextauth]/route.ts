ec0b004a0a0522424ce52eaa12068d22ffd8829f
MarcioJorgeMelo <marciojorgemedeirosmelofilho@gmail.com>
2025-06-02 19:08:55 -0300

    fix: reajusting config´s auth js to user data via server

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,26 +1,6 @@
-import { db } from "@/lib/prisma"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { AuthOptions } from "next-auth"
-import { Adapter } from "next-auth/adapters"
-import GoogleProvider from "next-auth/providers/google"
+import { authOptions } from "@/lib/auth"
+import NextAuth from "next-auth"
 
-export const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(db) as Adapter,
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-  callbacks: {
-    async session({ session, user }) {
-      session.user = {
-        ...session.user,
-        id: user.id,
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      } as any
-      return session
-    },
-  },
-  secret: process.env.NEXT_AUTH_SECRET,
-}
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
